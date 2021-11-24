3eeb041aab40023192564ffd4e30907848112eb5
jkalnins81 <j_kalnins@hotmail.com>
2021-11-24 10:22:57 +0100

    Updated PlayerJump.cs

--- Assets/Scripts/Player/PlayerJump.cs
+++ Assets/Scripts/Player/PlayerJump.cs
@@ -7,13 +7,13 @@ public class PlayerJump : MonoBehaviour
     public float velocity;
     private bool grounded = true;
     public float jumpHeight = 2f;
-    // Start is called before the first frame update
+    
     void Start()
     {
 
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         if (Input.GetButtonDown("Jump") && grounded)
@@ -31,29 +31,24 @@ public class PlayerJump : MonoBehaviour
     {
         while (transform.localPosition.y < 1.2f + jumpHeight)
         {
-            // Y axis up
             transform.localPosition += (Vector3) Vector2.up * 0.2f; // Time.deltaTime not need because not actually running in Update
 
             yield return new WaitForEndOfFrame();
         }    
 
-        yield return new WaitForSeconds(0.1f); // hangtime in air
+        yield return new WaitForSeconds(0.3f); // hangtime in air
 
         while (transform.localPosition.y > 1.2f)
         {
-            // Y axis up
+            
             transform.localPosition += (Vector3)Vector2.down * 0.2f; // Time.deltaTime not need because not actually running in Update
 
             yield return new WaitForEndOfFrame();
         }
 
-        // Y axis down
+        
 
         grounded = true;
     }
-
-    
-    // new function Inemueration (Name ie. Jump)
-    // grounded = true;
 
 }
