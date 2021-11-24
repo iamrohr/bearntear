a1fa341b2283678302d798eb5d09057b3630bfec
jkalnins81 <j_kalnins@hotmail.com>
2021-11-24 10:19:44 +0100

    PlayerJump.cs updated

--- Assets/Scripts/Player/PlayerJump.cs
+++ Assets/Scripts/Player/PlayerJump.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerJump : MonoBehaviour
@@ -5,6 +6,7 @@ public class PlayerJump : MonoBehaviour
     public float jumpSpeed;
     public float velocity;
     private bool grounded = true;
+    public float jumpHeight = 2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,14 +16,44 @@ public class PlayerJump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && grounded)
         {
             grounded = false;
-            transform.Translate(Vector2.up * 5 * Time.deltaTime);
+            StartCoroutine(Jump());
+
         }
-        if (!grounded)
-        {
-            //transform.Translate(Vector2.down * 9.81f * Time.deltaTime);
-        }
+       
+
+
     }
+
+    private IEnumerator Jump()
+    {
+        while (transform.localPosition.y < 1.2f + jumpHeight)
+        {
+            // Y axis up
+            transform.localPosition += (Vector3) Vector2.up * 0.2f; // Time.deltaTime not need because not actually running in Update
+
+            yield return new WaitForEndOfFrame();
+        }    
+
+        yield return new WaitForSeconds(0.1f); // hangtime in air
+
+        while (transform.localPosition.y > 1.2f)
+        {
+            // Y axis up
+            transform.localPosition += (Vector3)Vector2.down * 0.2f; // Time.deltaTime not need because not actually running in Update
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        // Y axis down
+
+        grounded = true;
+    }
+
+    
+    // new function Inemueration (Name ie. Jump)
+    // grounded = true;
+
 }
