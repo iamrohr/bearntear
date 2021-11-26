02bdd4485d395a9c5bd1a36948e89f24257738b1
RicheyDev <90191926+RicheyDev@users.noreply.github.com>
2021-11-26 11:22:00 +0100

    tear bar

--- Assets/Scripts/Player/TearBarOnPlayer.cs
+++ Assets/Scripts/Player/TearBarOnPlayer.cs
@@ -4,18 +4,14 @@ using UnityEngine;
 
 public class TearBarOnPlayer : MonoBehaviour
 {
-    public int maxHealth = 100;
-    public int currentEnergy;
-
-    //ndra denna
-    public int currentHealth;
-
     public HealthBar tearBar;
 
+    public int maxHealth = 100;
+    public int currentHealth;
+    
     //Counter
-
     float timeElapsed = 100; //Count time down from 100 
-    float timeSpeed = 10;    //Acceleration
+    public float timeSpeed = 1;    //Acceleration
 
     void Start()
     {
@@ -25,29 +21,14 @@ public class TearBarOnPlayer : MonoBehaviour
 
     void Update()
     {
-        //decrease energy
-        if (GameObject.FindGameObjectWithTag("Player") != null)
-        {
-            if (currentEnergy >= 0)
-            {
-                timeElapsed -= timeSpeed * Time.deltaTime;
-                currentEnergy = (int)timeElapsed;
-                tearBar.SetHealth(currentHealth);
-
-            }
-
+        //decrease tear
+        if (currentHealth > 0)
+        { 
+            timeElapsed -= timeSpeed * Time.deltaTime;
+            currentHealth = (int)timeElapsed;
+            tearBar.SetHealth(currentHealth);
         }
-
-        //increase energy
-        if (GameObject.FindGameObjectWithTag("Possessed") != null)
-        {
-            if (currentEnergy <= 100)
-            {
-                timeElapsed += timeSpeed * Time.deltaTime;
-                currentHealth = (int)timeElapsed;
-                tearBar.SetHealth(currentHealth);
-            }
-
-        }
+        
     }
+
 }
