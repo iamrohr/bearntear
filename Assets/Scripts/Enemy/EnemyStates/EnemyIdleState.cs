ed15293a0f0394cecadf5fc96c4179fb95643b74
RicheyDev <90191926+RicheyDev@users.noreply.github.com>
2021-11-29 11:01:11 +0100

    Update EnemyIdleState.cs

--- Assets/Scripts/Enemy/EnemyStates/EnemyIdleState.cs
+++ Assets/Scripts/Enemy/EnemyStates/EnemyIdleState.cs
@@ -11,11 +11,7 @@ public class EnemyIdleState : EnemyBaseState
     {
         Debug.Log("Hello, I am the Enter state of the Idle state using the Enemy State Manager");
 
-        player = GameObject.Find("Player");
 
-        this.startPosition = new Vector2(player.transform.position.x, this.transform.position.y);
-
-        attackBox.SetActive(false);
     }
 
     public override void UpdateState(EnemyStateManager enemy)
