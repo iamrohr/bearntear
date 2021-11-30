0272453998ded9247ea5289e54c5eaaa2d3ee433
RicheyDev <90191926+RicheyDev@users.noreply.github.com>
2021-11-30 11:15:51 +0100

    Update EnemyReturnHome.cs

--- Assets/Scripts/Enemy/EnemyStates/EnemyReturnHome.cs
+++ Assets/Scripts/Enemy/EnemyStates/EnemyReturnHome.cs
@@ -4,29 +4,28 @@ public class EnemyReturnHomeState : EnemyBaseState
 {
     public override void EnterState(EnemyStateManager enemy)
     {
-        Debug.Log("Dang, where did she go, I better get back home");
         enemy.attackBox.SetActive(false);
     }
 
     public override void UpdateState(EnemyStateManager enemy)
     {
+
         Vector2 currentposition = enemy.transform.position;
-
         float step = enemy.returnSpeed * Time.deltaTime;
-
-
-        //Lägg till så den flippar sprite render. 
-
-        //Gör en Gamecomponent som är public som är just den spriten.
-        
-        enemy.transform.localScale = new Vector2(1, 1);
-
 
         enemy.transform.position = Vector2.MoveTowards(enemy.transform.position, enemy.startPosition, step);
 
+        //Flips the enemy towards the start position.
+        if (enemy.transform.position.x < enemy.startPosition.x)
+        {
+            enemy.transform.localScale = new Vector2(1, 1);
+        }
+        else
+            enemy.transform.localScale = new Vector2(-1, 1);
+
+
         if (currentposition.y == enemy.startPosition.y)
         {
-            Debug.Log("Borde vara i idle nu");
             enemy.SwitchState(enemy.IdleState);
         }
 
