2870772a53a618304cf2be1ea3a4348ade1c6343
RicheyDev <90191926+RicheyDev@users.noreply.github.com>
2021-12-01 09:54:31 +0100

    Added random Cotton

--- Assets/Scripts/GiveLife.cs
+++ Assets/Scripts/GiveLife.cs
@@ -2,7 +2,15 @@ using UnityEngine;
 
 public class GiveLife : MonoBehaviour
 {
+    private int rand;
+    public Sprite[] cottonSprites;
     public int giveLife;
+
+    private void Start()
+    {
+        rand = Random.Range(0, cottonSprites.Length);
+        GetComponent<SpriteRenderer>().sprite = cottonSprites[rand];
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
