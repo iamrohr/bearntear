b66927289d2a83a48c2516e21f45599fb5ed1fdc
RicheyDev <90191926+RicheyDev@users.noreply.github.com>
2021-12-03 09:15:07 +0100

    commit

--- Assets/Scripts/GiveLife.cs
+++ Assets/Scripts/GiveLife.cs
@@ -10,19 +10,21 @@ public class GiveLife : MonoBehaviour
     [SerializeField] private float cottonPickupSoundVolume= 1f;
 
     public GameObject audioManager;
+    public AudioManager audioManagerScript;
 
     private void Start()
     {
         rand = Random.Range(0, cottonSprites.Length);
         GetComponent<SpriteRenderer>().sprite = cottonSprites[rand];
         audioManager = GameObject.FindGameObjectWithTag("Audio Manager");
+        audioManagerScript = audioManager.GetComponent<AudioManager>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-           if(cottonPickupSound) audioManager.GetComponent<AudioManager>().sfxAudioSource.PlayOneShot(cottonPickupSound, cottonPickupSoundVolume);
+           if(cottonPickupSound) audioManagerScript.sfxAudioSource.PlayOneShot(cottonPickupSound, cottonPickupSoundVolume);
 
             other.GetComponent<Player>().GetLife(giveLife);
             Destroy(gameObject);
