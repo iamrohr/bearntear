fb937793e00c163ce13f6e48a7afe9a798a2dac9
jkalnins81 <j_kalnins@hotmail.com>
2021-12-05 20:52:08 +0100

    Fixed camera clamping (not 100% but working well)

--- Assets/Scripts/Camera/MoveCamera.cs
+++ Assets/Scripts/Camera/MoveCamera.cs
@@ -12,8 +12,9 @@ public class MoveCamera : MonoBehaviour
     float deltaY;
 
     public float smoothFloat;
+    public float clampedLeftPos = -32.5f;
+    public float clampedRightPos = 32f;
 
-    
     void Start()
     {
         
@@ -25,22 +26,28 @@ public class MoveCamera : MonoBehaviour
         //convert target pos to 2D
         
         targetScreenPos = Camera.main.WorldToScreenPoint(targetPosition.position);
+
+        if(targetPosition.position.x >= clampedLeftPos && targetPosition.position.x <= clampedRightPos)
+        {
+            if (targetScreenPos.x > (Screen.width / 2) + camWindowDimension)
+            {
+                deltaX = targetScreenPos.x - ((Screen.width / 2) + camWindowDimension);
+
+                transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x + deltaX, transform.position.y, transform.position.z), smoothFloat);
+            }
+        }
+
+        if(targetPosition.position.x >= clampedLeftPos && targetPosition.position.x <= clampedRightPos)
+        {
+            if (targetScreenPos.x < (Screen.width / 2) - camWindowDimension)
+            {
+                deltaX = targetScreenPos.x - ((Screen.width / 2) - camWindowDimension);
+
+                transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x + deltaX, transform.position.y, transform.position.z), smoothFloat);
+            }
+        }
+
         
-        if (targetScreenPos.x > (Screen.width / 2) + camWindowDimension)
-        {
-            deltaX = targetScreenPos.x - ((Screen.width / 2) + camWindowDimension);
-            
-            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x + deltaX, transform.position.y, transform.position.z), smoothFloat);
-
-        }
-
-        if (targetScreenPos.x < (Screen.width / 2) - camWindowDimension)
-        {
-            deltaX = targetScreenPos.x - ((Screen.width / 2) - camWindowDimension);
-            
-            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x + deltaX, transform.position.y, transform.position.z), smoothFloat);
-
-        }
 
         //if (targetScreenPos.y > (Screen.height / 2) + camWindowDimension)
         //{
