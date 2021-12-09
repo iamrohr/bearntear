26189d6350a10959f491d7929c2976e93b198a09
jkalnins81 <j_kalnins@hotmail.com>
2021-12-09 14:05:30 +0100

    TearMode under construction

--- Assets/Scripts/Player/TearMode.cs
+++ Assets/Scripts/Player/TearMode.cs
@@ -4,8 +4,39 @@ using UnityEngine;
 
 public class TearMode : MonoBehaviour
 {
-    private void Update()
+    public TearBarOnPlayer tearBarOnPlayerScript;
+    public Camera mainCamera;
+
+    CameraShake cameraShake;
+
+    private void Start()
     {
-        // if(Input.)
+        cameraShake = GameObject.Find("CameraHolder").GetComponent<CameraShake>();
+
+    }
+
+    public IEnumerator TearModeStart()
+    {
+        mainCamera.orthographicSize = 4.0f;
+        Time.timeScale = 0;
+        // knockBack effect
+        // playSound
+        // particleEffect
+        StartCoroutine(cameraShake.Shake(1f, 0.4f));
+
+        yield return new WaitForSeconds(1f);
+
+
+    }
+
+
+        private void Update()
+    {
+        if (tearBarOnPlayerScript.currentTear == tearBarOnPlayerScript.maxTear)
+
+            if (Input.GetButton("TearMode"))
+            {
+                StartCoroutine(TearModeStart());
+            }
     }
 }
