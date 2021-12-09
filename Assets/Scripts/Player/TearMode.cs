a95aca65339406759ea87e23e9926bc5453aa6ed
jkalnins81 <j_kalnins@hotmail.com>
2021-12-09 13:23:17 +0100

    Cleaned up scripts

---
+++ Assets/Scripts/Player/TearMode.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TearMode : MonoBehaviour
+{
+    private void Update()
+    {
+        // if(Input.)
+    }
+}
