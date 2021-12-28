4ffb221d9559028e26cc618e94cd3ec7be775007
jkalnins81 <j_kalnins@hotmail.com>
2021-12-28 11:42:52 +0100

    SpawnPoint2 CameraLock + colliders working

--- Assets/Scripts/Camera/CameraSpawnLock.cs
+++ Assets/Scripts/Camera/CameraSpawnLock.cs
@@ -4,34 +4,55 @@ using UnityEngine;
 
 public class CameraSpawnLock : MonoBehaviour
 {
-    public GameObject spawner;
+    public GameObject spawner1;
     public GameObject spawnPoint1LeftCollider;
     public GameObject spawnPoint1RightCollider;
 
+    public GameObject spawner2;
+    public GameObject spawnPoint2LeftCollider;
+    public GameObject spawnPoint2RightCollider;
+
     MoveCamera moveCameraScript;
-    WaveSpawner waveSpawnerScript;
+    WaveSpawner waveSpawner1Script;
+    WaveSpawner waveSpawner2Script;
 
     private void Start()
     {
-        waveSpawnerScript = spawner.GetComponent<WaveSpawner>();
+        waveSpawner1Script = spawner1.GetComponent<WaveSpawner>();
+        waveSpawner2Script = spawner2.GetComponent<WaveSpawner>();
         moveCameraScript = this.gameObject.GetComponent<MoveCamera>();
     }
 
     private void Update()
     {
-        if (transform.position.x > 34 && waveSpawnerScript.ableToSpawn)
+        if (transform.position.x > 34 && waveSpawner1Script.ableToSpawn)
         {
             moveCameraScript.enabled = false;
             spawnPoint1LeftCollider.SetActive(true);
             spawnPoint1RightCollider.SetActive(true);
-            // enable left (x pos 23.5) & right (x pos 45.3) collider from first spawn position
+            
         }
 
-        if(!waveSpawnerScript.ableToSpawn)
+        if(transform.position.x > 34 && !waveSpawner1Script.ableToSpawn)
         {
             moveCameraScript.enabled = true;
             spawnPoint1LeftCollider.SetActive(false);
             spawnPoint1RightCollider.SetActive(false);
+        }
+
+        if (transform.position.x > 99.5 && waveSpawner2Script.ableToSpawn)
+        {
+            moveCameraScript.enabled = false;
+            spawnPoint2LeftCollider.SetActive(true);
+            spawnPoint2RightCollider.SetActive(true);
+            // enable left (x pos 88.46) & right (x pos 110.37) collider from first spawn position
+        }
+
+        if (transform.position.x > 99.5 && !waveSpawner2Script.ableToSpawn)
+        {
+            moveCameraScript.enabled = true;
+            spawnPoint2LeftCollider.SetActive(false);
+            spawnPoint2RightCollider.SetActive(false);
         }
     }
 
