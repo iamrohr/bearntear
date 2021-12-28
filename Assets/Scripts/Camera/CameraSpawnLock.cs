92c076b4ccf66e14c97a1a9928a2b6c26e80d281
jkalnins81 <j_kalnins@hotmail.com>
2021-12-28 11:08:42 +0100

    SpawnPoint1 cameraLock working

--- Assets/Scripts/Camera/CameraSpawnLock.cs
+++ Assets/Scripts/Camera/CameraSpawnLock.cs
@@ -4,5 +4,36 @@ using UnityEngine;
 
 public class CameraSpawnLock : MonoBehaviour
 {
-    
+    public GameObject spawner;
+    public GameObject spawnPoint1LeftCollider;
+    public GameObject spawnPoint1RightCollider;
+
+    MoveCamera moveCameraScript;
+    WaveSpawner waveSpawnerScript;
+
+    private void Start()
+    {
+        waveSpawnerScript = spawner.GetComponent<WaveSpawner>();
+        moveCameraScript = this.gameObject.GetComponent<MoveCamera>();
+    }
+
+    private void Update()
+    {
+        if (transform.position.x > 34 && waveSpawnerScript.ableToSpawn)
+        {
+            moveCameraScript.enabled = false;
+            spawnPoint1LeftCollider.SetActive(true);
+            spawnPoint1RightCollider.SetActive(true);
+            // enable left (x pos 23.5) & right (x pos 45.3) collider from first spawn position
+        }
+
+        if(!waveSpawnerScript.ableToSpawn)
+        {
+            moveCameraScript.enabled = true;
+            spawnPoint1LeftCollider.SetActive(false);
+            spawnPoint1RightCollider.SetActive(false);
+        }
+    }
+
+
 }
