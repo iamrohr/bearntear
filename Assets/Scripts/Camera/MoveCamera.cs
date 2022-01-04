ae2eca3dba3422060067db649dc8be1cc481cd8a
jkalnins81 <j_kalnins@hotmail.com>
2022-01-04 17:53:55 +0100

    MoveCamera prep for Sewer

--- Assets/Scripts/Camera/MoveCamera.cs
+++ Assets/Scripts/Camera/MoveCamera.cs
@@ -39,6 +39,17 @@ public class MoveCamera : MonoBehaviour
             cameraSpawnLock = gameObject.GetComponent<CameraSpawnLock>();
             cameraSpawnLock.enabled = false;
         }
+
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Sewer"))
+        {
+            clampedLeftPos = -65.8909f;
+            clampedRightPos = -20f;
+            transform.position = new Vector3(transform.position.x, -1.08f, transform.position.z);
+            cameraSpawnLock = gameObject.GetComponent<CameraSpawnLock>();
+            cameraSpawnLock.enabled = false;
+        }
+
+
     }
 
     void FixedUpdate()
