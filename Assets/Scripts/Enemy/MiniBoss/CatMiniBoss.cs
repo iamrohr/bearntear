2719c93c145b44da6f63e382fecf1a1e3f100a31
jkalnins81 <j_kalnins@hotmail.com>
2022-01-04 08:17:02 +0100

    RoofTop MiniBoss Spawner + Music - changes to MiniBoss prefabs

--- Assets/Scripts/Enemy/MiniBoss/CatMiniBoss.cs
+++ Assets/Scripts/Enemy/MiniBoss/CatMiniBoss.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class CatMiniBoss : MonoBehaviour
 {
-    
+    public GameObject catMiniBoss;
+    public GameObject player;
+
+    private void Update()
+    {
+        if (GameObject.FindGameObjectsWithTag("Enemy").Length <= 0)
+        {
+            Instantiate(catMiniBoss, new Vector3(125.31f, -2.63f, 0f), player.transform.rotation);
+        }
+    }
 }
