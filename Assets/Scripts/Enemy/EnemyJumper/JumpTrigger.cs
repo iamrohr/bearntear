9175a59e5940832b91436cf13f3d59dfd1bcdf55
RicheyDev <90191926+RicheyDev@users.noreply.github.com>
2022-01-04 13:36:10 +0100

    Refeence bug fixxed & Removed Debug Logs

--- Assets/Scripts/Enemy/EnemyJumper/JumpTrigger.cs
+++ Assets/Scripts/Enemy/EnemyJumper/JumpTrigger.cs
@@ -17,14 +17,11 @@ public class JumpTrigger : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-
-        //Debug.Log("GameObject1 collided with " + other.name);
-
         if (other.gameObject.tag == "Player")
         {
-            //Debug.Log("Jaharrrrr I will Jump on you");
             enemyFollowPath.coroutineAllowed = true;
             enemySprite.enabled = true;
+            Destroy(gameObject, 2f);
         }
     }
 }
