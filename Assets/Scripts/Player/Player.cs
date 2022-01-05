775e3237a4c8e1ea23a94aa4982f8db648fa39cb
jkalnins81 <j_kalnins@hotmail.com>
2022-01-05 16:18:31 +0100

    RoofTop Death to GameOver2Scene Start from RoofTop again

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -20,8 +20,6 @@ public class Player : MonoBehaviour, IDamageable
 
     GameObject persistentObject;
 
-    public bool diedOnRoofTop;
-
     private void Awake()
     {
         healthBar = GameObject.Find("HealthBar").GetComponent<HealthBar>();
@@ -54,14 +52,12 @@ public class Player : MonoBehaviour, IDamageable
 
         if (currentHealth <= 0 && SceneManager.GetActiveScene() == SceneManager.GetSceneByName("RoofTop"))
         {
-            diedOnRoofTop = true;
             Destroy(persistentObject);
-            SceneManager.LoadScene("GameOver");
+            SceneManager.LoadScene("GameOver2");
         }
 
         if (currentHealth <= 0 && SceneManager.GetActiveScene() != SceneManager.GetSceneByName("RoofTop"))
         {
-            diedOnRoofTop = false;
             Destroy(persistentObject);
             SceneManager.LoadScene("GameOver");
         }
